70e0ebcd82ec577fb664b147f0329e82bf68218b
jeremy-kiss <jeremyjkiss@gmail.com>
2024-06-18 15:09:15 -0600

    load thumbnails in grid, full size in modal

--- js/modal.js
+++ js/modal.js
@@ -5,18 +5,16 @@ var img = document.getElementsByClassName("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 
- for(i=0;i< img.length;i++)
-   {    
-    img[i].onclick = function(){
-
+for(i = 0; i < img.length; i++) {
+  img[i].onclick = function() {
     modal.style.display = "block";
-       modalImg.src = this.src;
-	   captionText.innerHTML = this.alt;
- }
+    modalImg.src = this.getAttribute("data-fullsize-src"); 
+    captionText.innerHTML = this.alt;
+  }
 }
 
 var span = document.getElementsByClassName("close")[0];
 
 span.onclick = function() { 
-   modal.style.display = "none";
+  modal.style.display = "none";
 }
